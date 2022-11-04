14987949ea886ceea8fe0ae51e43c72ca2d60c3d
Jeremiah Swearingen <jswearingen18@gmail.com>
2022-11-03 21:19:51 -0500

    added models to the project

---
+++ TECHFL!CK/models/User.js
@@ -0,0 +1,43 @@
+const { Model, DataTypes } = require("sequelize");
+bcrypt = require("bcrypt");
+sequelize = require("../config/connection");
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isAlphaNumeric: true,
+        len: [6, 15],
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "user",
+  }
+);
+
+module.exports = User;
