e26599b308b255938b1e86fdcbc5011856766809
David Herring <davidherring@Davids-MBP.home>
2022-11-07 21:17:49 -0600

    homeRoute

--- TECHFL!CK/controllers/api/userRoutes.js
+++ TECHFL!CK/controllers/api/userRoutes.js
@@ -1,6 +1,29 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+
+router.get('/', async (req, res) => {
+  try {
+      const userData = await User.findAll({
+        include: [
+          {
+            model: User,
+            attributes: ['id'],
+          },
+        ],
+      })
+    const user = userData.map((user) => user.get({ plain: true }));
+
+    res.render('profile', { 
+      user, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+  
+
 router.post('/', async (req, res) => {
   try {
     const usersData = await User.create(req.body);
@@ -20,23 +43,28 @@ router.post('/login', async (req, res) => {
     const usersData = await User.findOne({ where: { email: req.body.email } });
 
     if (!usersData) {
-      res.status(400).json({ message: 'Login failed, please try again' });
+      res
+        .status(400)
+        .json({ message: 'Login failed, please try again' });
       return;
     }
 
     const correctPassword = await usersData.checkPassword(req.body.password);
 
     if (!correctPassword) {
-      res.status(400).json({ message: 'Login failed, please try again' });
+      res
+        .status(400)
+        .json({ message: 'Login failed, please try again' });
       return;
     }
 
     req.session.save(() => {
       req.session.userId = usersData.id;
       req.session.logged_in = true;
-
+      
       res.json({ user: usersData, message: 'Successfully logged in!' });
     });
+
   } catch (err) {
     res.status(400).json(err);
   }
