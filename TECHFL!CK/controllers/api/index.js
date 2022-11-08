354b9897639abab8fff399f7f97fe0635fbd331f
Jeremiah Swearingen <jswearingen18@gmail.com>
2022-11-07 19:51:42 -0600

    working routes for login

--- TECHFL!CK/controllers/api/index.js
+++ TECHFL!CK/controllers/api/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const jokesRoutes = require('./jokesRoutes');
-const likesRoutes = require('./likesRoutes');
+//const likesRoutes = require('./likesRoutes');
 
 router.use('/users', userRoutes);
 router.use('/jokes', jokesRoutes);
-router.use('/likes', likesRoutes);
+//router.use('/likes', likesRoutes);
 
 module.exports = router;
