18a12b1a002942038c002663aeb4318f8152db13
Jeremiah Swearingen <jswearingen18@gmail.com>
2022-11-07 19:15:54 -0600

    added jokesroutes

--- TECHFL!CK/controllers/api/jokesRoutes.js
+++ TECHFL!CK/controllers/api/jokesRoutes.js
@@ -1,3 +1,38 @@
 const router = require('express').Router();
 const { Jokes } = require('../../models');
-const validateUser = 
+const validateUser = require('../../utils/auth');
+
+router.post('/', validateUser, async (req, res) => {
+  try {
+    const newJoke = await Jokes.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newJoke);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.delete('/:id', validateUser, async (req, res) => {
+  try {
+    const jokeData = await Jokes.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!jokeData) {
+      res.status(404).json({ message: 'No jokes were found with this id' });
+      return;
+    }
+
+    res.status(200).json(jokeData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
