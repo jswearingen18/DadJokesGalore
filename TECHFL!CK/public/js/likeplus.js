598e756db658dfe3d7ee3689ca4b67ccc3eb7b7d
David Greenhouse <dgreenhouse96@gmail.com>
2022-11-10 20:27:51 -0600

    updated create account

--- TECHFL!CK/public/js/likeplus.js
+++ TECHFL!CK/public/js/likeplus.js
@@ -1,9 +1,10 @@
 const likeButton = document.getElementsByClassName("like");
 const likeCount = document.getElementById("likes");
-
+debugger
 var count = 0;
 
 likeButton.addEventListener("click", () => {
     count++;
     likeCount.innerHTML = count;
+    console.log("working");
 });
