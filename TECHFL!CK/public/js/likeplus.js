6cd98b3f9fce117f15c82ea5c4e852e261f65abd
David Herring <davidherring@Davids-MBP.home>
2022-11-10 21:50:53 -0600

    fixing likebtn

--- TECHFL!CK/public/js/likeplus.js
+++ TECHFL!CK/public/js/likeplus.js
@@ -1,32 +1,32 @@
 
-async function likeButton(id,likes) {
-    const url = `api/jokes/${id}`
-    const body = {
-        likes:likes
-    }
-        const response = await fetch(url,{method:"PUT",body:JSON.stringify(body)});
-        const json = await JSON.stringify(response.json());
-        return json;
-}
+// async function likeButton(id,likes) {
+//     const url = `api/jokes/${id}`
+//     const body = {
+//         likes:likes
+//     }
+//         const response = await fetch(url,{method:"PUT",body:JSON.stringify(body)});
+//         const json = await JSON.stringify(response.json());
+//         return json;
+// }
 
 
-const likeButton = document.getElementsByClassName("like");
+ const likeButton = document.getElementsByClassName("like");
 const likeCount = document.getElementById("likes");
 debugger
 var count = 0;
 
-likeButton.addEventListener("click", () => {
-    count++;
+ likeButton.addEventListener("click", () => {
+     count++;
     likeCount.innerHTML = count;
     console.log("working");
-});
+ });
 
-document.querySelector('.like-button').addEventListener('click', likeButton);
+ document.querySelector('.like-button').addEventListener('click', likeButton);
 
-var count = 0;
+ var count = 0;
 
-const likeButton = () => {
-  console.log('Hello');
-};
+ const likeButton = () => {
+   console.log('Hello');
+ };
 
 
