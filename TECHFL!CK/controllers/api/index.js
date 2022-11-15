02540e595da92cdcc6edd8f19cd9fbfbe0fdab3e
Tristan Barron <tristan_wilson_63@yahoo.com>
2022-11-15 15:37:06 -0600

    bug fix

--- TECHFL!CK/controllers/api/index.js
+++ TECHFL!CK/controllers/api/index.js
@@ -2,12 +2,12 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const jokesRoutes = require('./jokesRoutes');
 
-const app = express();
-app.use(express.json());
+// const app = express();
+// app.use(express.json());
 
 router.use('/users', userRoutes);
 router.use('/jokes', jokesRoutes);
 
-app.use(router),
+// app.use(router),
 
 module.exports = router;
