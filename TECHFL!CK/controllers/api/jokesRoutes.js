e313829f78d3e8ff549656c71b91dc77b6ce1933
Jeremiah Swearingen <jswearingen18@gmail.com>
2022-11-15 11:07:12 -0600

    updates routes

--- TECHFL!CK/controllers/api/jokesRoutes.js
+++ TECHFL!CK/controllers/api/jokesRoutes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const { Jokes, User } = require('../../models');
 const validateUser = require('../../utils/auth');
 
+
 router.post('/', validateUser, async (req, res) => {
   try {
     const newJoke = await Jokes.create({
@@ -18,9 +19,8 @@ router.post('/', validateUser, async (req, res) => {
 });
 
 router.put('/:id', async (req, res) => {
-  const actions = req.params;
-  console.log("line 22", actions);
-   if (actions === 'likes') {
+  console.log("line 22", req.body);
+  // if (action === 'likes') {
   try {
     const likesData = await Jokes.increment(
       {
@@ -32,30 +32,30 @@ router.put('/:id', async (req, res) => {
         },
       }
     );
-    console.log(likesData);
     res.status(200).json(likesData);
   } catch (err) {
     res.status(400).json(err);
   }
-} else if (actions === 'dislikes') {
-    try {
-      const likesData = await Jokes.increment(
-        {
-          dislikes: 1,
-        },
-        {
-          where: {
-            id: req.params.id,
-          },
-        }
-      );
-      console.log(likesData);
-      res.status(200).json(likesData);
-    } catch (err) {
-      res.status(400).json(err);
-    }
-  }
-  });
+});
+// } else if (action === 'dislikes') {
+//     try {
+//       const dislikesData = await Jokes.increment(
+//         {
+//           dislikes: 1,
+//         },
+//         {
+//           where: {
+//             id: req.params.id,
+//           },
+//         }
+//       );
+//       console.log(dislikesData);
+//       res.status(200).json(likesData);
+//     } catch (err) {
+//       res.status(400).json(err);
+//     }
+//   }
+//   });
 
 router.delete('/:id', validateUser, async (req, res) => {
   try {
