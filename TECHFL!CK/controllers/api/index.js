97e2dcd03c44a697e6ec59b816d523dc249bb27a
Jeremiah Swearingen <jswearingen18@gmail.com>
2022-11-15 12:42:56 -0600

    heroku

--- TECHFL!CK/controllers/api/index.js
+++ TECHFL!CK/controllers/api/index.js
@@ -2,12 +2,7 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const jokesRoutes = require('./jokesRoutes');
 
-const app = express();
-app.use(express.json());
-
 router.use('/users', userRoutes);
 router.use('/jokes', jokesRoutes);
-
-app.use(router),
 
 module.exports = router;
